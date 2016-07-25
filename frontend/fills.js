f5d088a70b569d35bea15897ad60515328c442c4
Stefan Hechenberger <stefan@nortd.com>
2016-07-25 11:39:42 +0200

    raster fill algo

--- frontend/fills.js
+++ frontend/fills.js
@@ -2,17 +2,49 @@
 
 
 function fills_add_by_color(color) {
-  pathidx = jobhandler.vector.colors.indexOf(color)
-  path = jobhandler.vector.paths[pathidx]
-  // bounds =
-  // fillcolor =
+  var pathidx = jobhandler.vector.colors.indexOf(color)
+  var path = jobhandler.vector.paths[pathidx]
+  var bounds = jobhandler.stats.paths[pathidx].bbox
+  var leadin = app_config_main.raster_leadin
+  var min_x = Math.max(bounds[0]-leadin, 0)
+  var max_x = Math.min(bounds[2]+leadin, app_config_main.workspace[0])
+  // generate a new color shifted from the old
+  var fillcolor = new paper.Color()
+  fillcolor = color
+  fillcolor.hue += 30
+  fillcolor = fillcolor.toCSS(true)
+
   console.log(color)
   console.log(path)
+
+  var line_delta = app_config_main.raster_size
+  // var num_lines = Math.floor((bounds[3]-bounds[1])/line_delta)
+  for (var y = bounds[1]+0.001; y <= bounds[3]; y+=line_delta) {
+    // for every fill line
+    // intersect with all segments of path
+    var intersections = []
+    for (var i = 0; i < path.length; i++) {
+      var polyline = path[i]
+      if (polyline.length > 1) {
+        pv = polyline[0]
+        for (var j = 1; j < polyline.length; j++) {
+          var v = polyline[j]
+          res = fills_intersect(min_x, y, max_x, y, pv[0], pv[1], v[0], v[1])
+          if (res.onSeg1 && res.onSeg2) {
+            intersections.push([res.x, res.y])
+          }
+          pv = v
+        }
+      }
+    }
+    // sort intersection points by x
+  }
 
   // algo
   //intersect horizontal lines with path
   // sort intersections by x
   // keep segments even to odd: 0st-1nd, 2rd-3th, ... (0 indexed)
+  // also make sure the feedrate and seekrate are equal
 
   // // update pass widgets
   // passes_clear()
@@ -30,8 +62,8 @@ function fills_intersect(Ax, Ay, Bx, By, Dx, Dy, Ex, Ey) {
     var denominator, a, b, numerator1, numerator2, result = {
         x: null,
         y: null,
-        onLine1: false,
-        onLine2: false
+        onSeg1: false,
+        onSeg2: false
     }
     denominator = ((Ey - Dy) * (Bx - Ax)) - ((Ex - Dx) * (By - Ay))
     if (denominator == 0) { return result }
@@ -46,11 +78,11 @@ function fills_intersect(Ax, Ay, Bx, By, Dx, Dy, Ex, Ey) {
     result.y = Ay + (a * (By - Ay))
     // if A-B is a segment and D-E is a ray, they intersect if:
     if (a > 0 && a < 1) {
-        result.onLine1 = true
+        result.onSeg1 = true
     }
     // if D-E is a segment and A-B is a ray, they intersect if:
     if (b > 0 && b < 1) {
-        result.onLine2 = true
+        result.onSeg2 = true
     }
     // if A-B and D-E are segments, they intersect if both are true
     return result
