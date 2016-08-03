dfc9271d498fa80f0e4149bc8907fd55902acdcd
Stefan Hechenberger <stefan@nortd.com>
2016-08-03 15:29:48 +0200

    scripts

--- scripts/upgrade_to_driveboardapp.sh
+++ scripts/upgrade_to_driveboardapp.sh
@@ -4,7 +4,7 @@ echo "Switching from LasaurApp to DriveboardApp ..."
 
 update-rc.d -f lasaurapp.sh remove
 
-cp driveboardapp.sh /etc/init.d/driveboardapp.sh
+cp /root/driveboardapp/driveboardapp.sh /etc/init.d/driveboardapp.sh
 chmod 755 /etc/init.d/driveboardapp.sh
 update-rc.d driveboardapp.sh defaults
 
