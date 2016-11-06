a2b98f435632473e63e3c4f13430d7f7816644bc
Stefan Hechenberger <stefan@nortd.com>
2016-11-06 13:39:17 +0100

    pxsize fix 2 because NaN is a f****g number in Javascript

--- frontend/passes.js
+++ frontend/passes.js
@@ -208,9 +208,9 @@ function passes_get_active() {
     var intensity = Math.round(parseFloat($(this).find("input.intensity").val()))
     var pxsize = parseFloat($(this).find("input.pxsize").val())
     var pass = {"items":[]}
-    if (typeof(feedrate) === 'number') { pass.feedrate = feedrate }
-    if (typeof(intensity) === 'number') { pass.intensity = intensity }
-    if (typeof(pxsize) === 'number') { pass.pxsize = pxsize }
+    if (typeof(feedrate) === 'number' && !isNaN(feedrate)) { pass.feedrate = feedrate }
+    if (typeof(intensity) === 'number' && !isNaN(intensity)) { pass.intensity = intensity }
+    if (typeof(pxsize) === 'number' && !isNaN(pxsize)) { pass.pxsize = pxsize }
     $(this).children('div.pass_colors').children('div').filter(':visible').each(function(k) {
       var idx = parseFloat($(this).find('.idxmem').text())
       pass.items.push(idx)
