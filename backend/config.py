9b44953029f7e5a5342878bd8e3342ce4befb826
Stefan Hechenberger <stefan@nortd.com>
2016-11-07 10:35:02 +0100

    platform dedections more forgiving

--- backend/config.py
+++ backend/config.py
@@ -99,23 +99,19 @@ conf['stordir'] = directory
 ### auto-check hardware
 #
 conf['hardware'] = 'standard'
-### check if running on BBB
-# also works on old Beaglebone if named 'lasersaur'
-# os.uname() on BBB:
-# ('Linux', 'lasersaur', '3.8.13-bone20',
-#  '#1 SMP Wed May 29 06:14:59 UTC 2013', 'armv7l')
 if sys.platform == "linux2":
-    uname = os.uname()
-    if uname[1] == 'lasersaur' or uname[2] == '3.8.13-bone20':
-        conf['hardware'] = 'beaglebone'
-    ### check if running on RaspberryPi
     try:
         import RPi.GPIO
         conf['hardware'] = 'raspberrypi'
     except ImportError:
-        pass
-    #
-    ###
+        # os.uname() on BBB:
+        # ('Linux', 'lasersaur', '3.8.13-bone20',
+        #  '#1 SMP Wed May 29 06:14:59 UTC 2013', 'armv7l')
+        if os.uname()[4].startswith('arm'):
+            conf['hardware'] = 'beaglebone'
+#
+###
+
 
 
 
