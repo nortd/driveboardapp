cbe3fd3bd396643831dab594b20269fe6488bc44
Stefan Hechenberger <stefan@nortd.com>
2017-01-13 10:44:36 +0100

    gui window in main thread

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,9 @@ import sys
 import os
 import time
 import argparse
+import Tkinter
 
+import window
 import web
 from config import conf
 
@@ -22,8 +24,12 @@ argparser.add_argument('-u', '--usbhack', dest='usbhack', action='store_true',
                        default=False, help='use usb reset hack (advanced)')
 argparser.add_argument('-b', '--browser', dest='browser', action='store_true',
                        default=False, help='launch interface in browser')
+argparser.add_argument('-c', '--cli', dest='cli', action='store_true',
+                       default=False, help='run without GUI window')
 args = argparser.parse_args()
 
+if not args.cli:
+    root = window.init()
 
 print "DriveboardApp " + conf['version']
 conf['usb_reset_hack'] = args.usbhack
@@ -34,6 +40,11 @@ web.start(threaded=args.threaded, browser=args.browser, debug=args.debug)
 if args.threaded:
     while 1:
         try:
+            if not args.cli:
+                try:
+                    root.update()
+                except Tkinter.TclError:
+                    break
             time.sleep(0.1)
         except KeyboardInterrupt:
             break
