3bfe5dbc4da83aee5b3d1355d9ca8621b46cd1fe
Stefan Hechenberger <stefan@nortd.com>
2017-01-20 15:20:25 +0100

    icons for win osx

--- deploy/make.py
+++ deploy/make.py
@@ -2,17 +2,15 @@ import os
 import sys
 
 if sys.platform == "darwin":
-    icon_opt = "--icon=icon.icns"
+    icon_opt = "--icon='icon.icns'"
 elif sys.platform == "win32":
-    icon_opt = "--icon=icon.ico"
+    icon_opt = "--icon='icon.ico'"
 else:
     icon_opt = ""
 
 if not sys.platform == "darwin":  # Linux Windows
-    os.system("python PyInstaller/pyinstaller.py --onefile --windowed --clean --noconfirm %s app.spec" % icon_opt)
+    cmd = "python PyInstaller/pyinstaller.py --onefile --windowed %s --clean --noconfirm app.spec" % icon_opt
 else:  # OSX
-    os.system("python PyInstaller/pyinstaller.py --onedir --windowed --clean --noconfirm %s app.spec" % icon_opt)
+    cmd = "python PyInstaller/pyinstaller.py --onedir --windowed %s --clean --noconfirm app.spec" % icon_opt
 
-# os.system("python PyInstaller/pyinstaller.py --onefile app.spec")
-# os.system("python PyInstaller/pyinstaller.py -w --clean --noconfirm app.spec")
-# os.system("python PyInstaller/pyinstaller.py --windowed --clean --noconfirm --hidden-import=pkg_resources ../backend/app.py")
+os.system(cmd)
