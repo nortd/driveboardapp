ba9ab715be01f8dd4cc47c53b1597e8d5ef265b6
J. Eric Townsend (jet) <jet@allartburns.org>
2017-11-11 00:23:37 +0000

    upgrade install and init.d scripts to ubuntu 16

--- scripts/downgrade_to_lasaurapp.sh
+++ scripts/downgrade_to_lasaurapp.sh
@@ -1,8 +1,11 @@
-
+#!/bin/sh
 
 echo "Switching from DriveboardApp to LasaurApp ..."
 
 update-rc.d -f driveboardapp.sh remove
+
+cp /root/driveboardapp/scripts/lasaurapp.sh /etc/init.d/lasaurapp.sh
+chmod 755 /etc/init.d/lasaurapp.sh
 update-rc.d lasaurapp.sh defaults
 
 echo "Done!"
